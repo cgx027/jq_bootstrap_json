50ce53af556c016d98eb3f3a6199c408942aadf5
Ted Chen <tedc@vmware.com>
2017-12-23 21:59:33 +0800

    first working version

--- index.js
+++ index.js
@@ -13,10 +13,73 @@ async function loadJSON(filename) {
   })
 }
 
+function createReleaseAnchor(element, version, link) {
+  const anchorTextTemplate = "<b>{element}</b>: <a href='{link}'>{version}</a>"
+  const anchorText = anchorTextTemplate
+    .replace('{link}', link)
+    .replace('{element}', element)
+    .replace('{version}', version)
+  return anchorText
+}
+
+function addReleases(releases) {
+  console.log('adding releases')
+  releases.forEach(function(release) {
+    console.log('add release: ', release)
+    const card = $("<div class='card' style='width: auto'></div></div>")
+    const cardBlock = $("<div class='card-block' id='card-block'></div>")
+    const cardTitle = $("<h4 class='class-title'>" + release.name + '</h4>')
+    const releaseDate = $(
+      "<p class='card-text'><b>release date</b>: " + release.date + '</p>'
+    )
+    const releaseDesc = $(
+      "<p class='card-text'><b>release description</b>: " +
+        release.description +
+        '</p>'
+    )
+    const biosRelease = $(
+      "<p class='card-text'>" +
+        createReleaseAnchor(
+          'BIOS',
+          release.BIOS.version,
+          release.BIOS.download
+        ) +
+        '</p>'
+    )
+    const bmcRelease = $(
+      "<p class='card-text'>" +
+        createReleaseAnchor(
+          'BMC',
+          release.BMC.version,
+          release.BMC.download
+        ) +
+        '</p>'
+    )
+    const nicRelease = $(
+      "<p class='card-text'>" +
+        createReleaseAnchor(
+          'NIC',
+          release.NIC.version,
+          release.NIC.download
+        ) +
+        '</p>'
+    )
+    cardBlock.append(cardTitle)
+    cardBlock.append(releaseDate)
+    cardBlock.append(releaseDesc)
+    cardBlock.append(biosRelease)
+    cardBlock.append(bmcRelease)
+    cardBlock.append(nicRelease)
+    card.append(cardBlock)
+    $('#releases').append(card)
+  })
+}
+
 async function main() {
   const releaseDataStr = await loadJSON('release.json')
   const releaseData = JSON.parse(releaseDataStr)
   console.log(releaseData)
+  addReleases(releaseData)
 }
 
 main()
